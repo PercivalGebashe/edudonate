3355fb5199072be38b4e9ebdad7739ab0c3c68b8
Percival_Gebashe <percivalgebashe@gmail.com>
2025-06-02 15:36:57 +0200

    Login logical error. Need to extract hash before comparing

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import { signToken } from "../utils/jwt.js";
 import User from "../models/User.js";
 
 const SALT_ROUNDS = 10;
@@ -45,3 +46,40 @@ export async function register(req, res) {
         res.status(500).json({message: "Server error during registration."});
     }
 };
+
+export async function login(req, res) {
+    const {email, password} = req.body;
+
+    console.log(`Details: ${email} ${password}`)
+
+    try{
+        if(!email){
+            return res.status(400).json({message: "Email cannot be empty"})
+        }
+
+        if(!password){
+            return res.status(400).json({message: "Password cannot be empty"})
+        }
+
+        const user = User.findByEmail(email);
+        console.log(`User: ${user}`)
+        
+        if(!user){
+            return res.status(401).json({message: "Invalid email or password"});
+        }
+
+        const isValid = await bcrypt.compare(password, user.password);
+        if(!isValid){
+            return res.status(401).json({message: "Invalid email or password"})
+        }
+
+        const token = signToken({userId: user.user_id, isAdmin: user.is_admin});
+
+        const {password_hash, ...safeUser} = user;
+
+        res.json({user: safeUser, token})
+    }catch(error){
+        console.log(`Login error:`, error);
+        res.status(500).json({error: "Server error during login"});
+    }
+}
--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -3,4 +3,12 @@ import dotenv from "dotenv"
 
 dotenv.config();
 
-const JWT_SECRET = process
+const JWT_SECRET = process.env.JWT_SECRET || "JWT TOKEN";
+
+export function signToken(payload){
+    return jwt.sign(payload, JWT_SECRET, {expiresIn: "1h"});
+}
+
+export function verifyToken(token){
+    return jwt.verify(token, JWT_SECRET);
+}
