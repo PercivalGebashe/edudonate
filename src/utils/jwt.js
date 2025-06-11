1f0524821db5f3ee62b78479a26d5efd7d3336ae
Percival_Gebashe <percivalgebashe@gmail.com>
2025-06-11 10:28:49 +0200

    updated jwt signature to include user role

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,14 +1,18 @@
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-export function signToken(payload){
-    return jwt.sign(payload, JWT_SECRET, {expiresIn: "1h"});
+export function signToken({ user_id, role }) {
+  return jwt.sign(
+    { userId: user_id, role }, // include both user ID and role
+    JWT_SECRET,
+    { expiresIn: "1h" }
+  );
 }
 
-export function verifyToken(token){
-    return jwt.verify(token, JWT_SECRET);
+export function verifyToken(token) {
+  return jwt.verify(token, JWT_SECRET);
 }
